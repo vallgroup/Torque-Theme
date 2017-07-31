9378e6c2f06a1b7c6328f06b85dc7014c459a8a4
Mario Vallejo <mario@vallgroup.com>
2017-07-31 08:07:11 -0500

    added spacing to return '';

--- functions.php
+++ functions.php
@@ -8,20 +8,20 @@
 	 * @subpackage 	Torque Theme
 	 * @author 			Torque
 	 */
-	
+
 /* ========================================================================================================================
-	
+
 	Required external files
-	
+
 	======================================================================================================================== */
 
 	require_once( 'external/torque-utilities.php' );
 	require_once( 'external/bs4navwalker.php' );
-	
+
 	/* ========================================================================================================================
-	
+
 	Add html 5 support to wordpress elements
-	
+
 	======================================================================================================================== */
 	add_theme_support( 'html5', array(
 		'comment-list',
@@ -32,21 +32,21 @@
 	) );
 
 	/* ========================================================================================================================
-	
+
 	Theme specific settings
-	
+
 	======================================================================================================================== */
 
 	add_theme_support('post-thumbnails');
-	
+
 	//add_image_size( 'name', width, height, crop true|false );
-	
+
 	register_nav_menus(array('primary' => 'Primary Navigation'));
 
 	/* ========================================================================================================================
-	
+
 	Actions and Filters
-	
+
 	======================================================================================================================== */
 
 	add_action( 'wp_enqueue_scripts', 'bootstrap_script_init' );
@@ -55,19 +55,19 @@
 	add_filter( 'body_class', array( 'TQ', 'add_slug_to_body_class' ) );
 
 	/* ========================================================================================================================
-	
+
 	Custom Post Types - include custom post types and taxonomies here e.g.
 
 	e.g. require_once( 'custom-post-types/your-custom-post-type.php' );
-	
+
 	======================================================================================================================== */
 
 
 
 	/* ========================================================================================================================
-	
+
 	Scripts
-	
+
 	======================================================================================================================== */
 
 	/**
@@ -78,7 +78,7 @@
 	 */
 
 	function bootstrap_script_init() {
-		
+
 		wp_register_script('bootstrap', get_template_directory_uri(). '/bootstrap/js/bootstrap.min.js', array( 'jquery' ), '3.3.7', true);
 		wp_enqueue_script('bootstrap');
 
@@ -86,7 +86,7 @@
 		wp_enqueue_style( 'bootstrap' );
 
 	}
-	
+
 	function custom_script_init(){
 
 		wp_register_script( 'scripts', get_template_directory_uri().'/dist/scripts.min.js', array( 'jquery' ), '0.0.1', true );
@@ -97,25 +97,25 @@
 
 	}
 	/* ========================================================================================================================
-	
+
 	Security & cleanup wp admin
-	
+
 	======================================================================================================================== */
-	
+
 	//remove wp version
 	function theme_remove_version() {
-	return '';
+		return '';
 	}
-	
+
 	add_filter('the_generator', 'theme_remove_version');
-	
+
 	//remove default footer text
 	function remove_footer_admin () {
 		echo "";
 	}
-	 
+
 	add_filter('admin_footer_text', 'remove_footer_admin');
-	
+
 	//remove wordpress logo from adminbar
 	function wp_logo_admin_bar_remove() {
 		global $wp_admin_bar;
@@ -123,48 +123,48 @@
 		/* Remove their stuff */
 		$wp_admin_bar->remove_menu('wp-logo');
 	}
-	
+
 	add_action('wp_before_admin_bar_render', 'wp_logo_admin_bar_remove', 0);
-	
+
 	// Remove default Dashboard widgets
 	function disable_default_dashboard_widgets() {
-	
+
 		//remove_meta_box('dashboard_right_now', 'dashboard', 'core');
 		remove_meta_box('dashboard_activity', 'dashboard', 'core');
 		remove_meta_box('dashboard_recent_comments', 'dashboard', 'core');
 		remove_meta_box('dashboard_incoming_links', 'dashboard', 'core');
 		remove_meta_box('dashboard_plugins', 'dashboard', 'core');
-	
+
 		remove_meta_box('dashboard_quick_press', 'dashboard', 'core');
 		remove_meta_box('dashboard_recent_drafts', 'dashboard', 'core');
 		remove_meta_box('dashboard_primary', 'dashboard', 'core');
 		remove_meta_box('dashboard_secondary', 'dashboard', 'core');
 	}
 	add_action('admin_menu', 'disable_default_dashboard_widgets');
-	
+
 	remove_action('welcome_panel', 'wp_welcome_panel');
-	
+
 	/* ========================================================================================================================
-	
+
 	Custom login
-	
+
 	======================================================================================================================== */
-	
+
 	// Add custom css
 	function my_custom_login() {
 		echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('stylesheet_directory') . '/custom-login-style.css" />';
 	}
 	add_action('login_head', 'my_custom_login');
-	
+
 	// Link the logo to the home of our website
 	function my_login_logo_url() {
 		return get_bloginfo( 'url' );
 	}
 	add_filter( 'login_headerurl', 'my_login_logo_url' );
-	
-	// Change the title text 
+
+	// Change the title text
 	function my_login_logo_url_title() {
 		return 'Torque Theme';
 	}
 	add_filter( 'login_headertitle', 'my_login_logo_url_title' );
-	
+
