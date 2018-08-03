e6a5bbe430a769ae4a5b7b95360869ea0def552e
mattwills8 <matt_wills8@hotmail.co.uk>
2018-08-03 23:40:17 +0200

    add webpack config and package.json to the boilerplate

--- plugins/lib/torque-plugin/webpack.config.js
+++ plugins/lib/torque-plugin/webpack.config.js
@@ -1,4 +1,4 @@
-const projectConfig = require('../../../config')
+const projectConfig = require('../../config')
 const path = require('path')
 const webpack = require('webpack')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
@@ -6,14 +6,14 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const srcDir = path.join(__dirname, 'src')
 const buildDir = path.join(
   projectConfig.root,
-  'wp-content/plugins/lib/torque-plugin'
+  'wp-content/plugins/<torque_plugin_slug>'
 )
 
 const config = {
   context: srcDir,
 
   entry: {
-    main: ['babel-polyfill', './index.js'],
+    main: ['babel-polyfill', './js/index.js'],
   },
 
   output: {
@@ -29,14 +29,45 @@ const config = {
         exclude: ['/node_modules/'],
         use: projectConfig.webpackDefaults.js.loaders,
       },
+      {
+        test: projectConfig.webpackDefaults.css.test,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader?sourceMap',
+          use: projectConfig.webpackDefaults.css.loaders,
+        }),
+      },
+      {
+        test: projectConfig.webpackDefaults.scss.test,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader?sourceMap',
+          // resolve-url-loader may be chained before sass-loader if necessary
+          use: projectConfig.webpackDefaults.scss.loaders,
+        }),
+      },
+      {
+        test: projectConfig.webpackDefaults.images.test,
+        use: projectConfig.webpackDefaults.images.loaders,
+      },
+      {
+        test: projectConfig.webpackDefaults.fonts.test,
+        use: projectConfig.webpackDefaults.fonts.loaders,
+      },
     ],
   },
 
   plugins: [
+    new ExtractTextPlugin({
+      filename: 'main.css',
+      publicPath: '/',
+      allChunks: true,
+      ignoreOrder: true,
+    }),
     new CopyWebpackPlugin([
       { from: path.join(srcDir, '**/*.php'), to: buildDir },
     ]),
   ],
+
+  devtool: 'source-map',
 }
 
 module.exports = config
