f2d3a50f8a63eb56428d95d8f8e7943f5d1d5c17
mattwills8 <matt_wills8@hotmail.co.uk>
2018-08-06 14:39:49 +0200

    start adding and arranging typography

--- plugins/torque-floor-plans/src/index.js
+++ plugins/torque-floor-plans/src/index.js
@@ -7,8 +7,6 @@ import './app/scss/main.scss'
 
 const entry = document.querySelectorAll('.torque-floor-plans-react-entry')
 
-console.log(entry)
-
 entry.forEach(entry => {
   if (entry) {
     ReactDOM.render(<App text={"Hello World"}/>, entry)
