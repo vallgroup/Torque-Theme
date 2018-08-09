d91ee69d50cc0b003d0c78f817f51e6addb9a9a8
mattwills8 <matt_wills8@hotmail.co.uk>
2018-08-09 17:02:25 +0200

    update acf to pro version

--- mu-plugins/load.php
+++ mu-plugins/load.php
@@ -1,6 +1,6 @@
 <?php
 
 require WPMU_PLUGIN_DIR.'/Premise-WP/premise.php';
-require WPMU_PLUGIN_DIR.'/advanced-custom-fields/acf.php';
+require WPMU_PLUGIN_DIR.'/advanced-custom-fields-pro/acf.php';
 
 ?>
