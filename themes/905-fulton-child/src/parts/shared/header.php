beae226336a6810401f89af447d427de2f1daf55
Mario Vallejo <mario@vallgroup.com>
2018-08-10 16:19:05 -0500

    added mobile nav structure

--- themes/905-fulton-child/src/parts/shared/header.php
+++ themes/905-fulton-child/src/parts/shared/header.php
@@ -1,8 +1,17 @@
 <nav>
   <div class="relative-container">
-    <?php
-      get_template_part( 'parts/shared/logo', 'dark');
-  	  get_template_part('parts/shared/header-parts/menu-items/menu-items', 'inline');
-    ?>
+
+  	<div class="site-logo">
+  		<?php get_template_part( 'parts/shared/logo', 'dark'); ?>
+  	</div>
+  	<a href="#toggle-nav" id="mobile-nav-toggle" class="mobile-nav-toggle">
+  		<span></span>
+  		<span></span>
+  		<span></span>
+  	</a>
+  	<div class="navigation" id="navigation">
+  		<?php get_template_part('parts/shared/header-parts/menu-items/menu-items', 'inline'); ?>
+  	</div>
+
   </div>
 </nav>
