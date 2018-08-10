71ab76599fd879252a3556510e8933b299eca691
mattwills8 <matt_wills8@hotmail.co.uk>
2018-08-11 00:10:34 +0200

    QA changes

--- themes/905-fulton-child/src/js/index.js
+++ themes/905-fulton-child/src/js/index.js
@@ -1,5 +1,12 @@
-import "../scss/main.scss"
+import '../scss/main.scss'
 
 if (!global._babelPolyfill) {
-	require('babel-polyfill');
+  require('babel-polyfill')
 }
+
+jQuery(window).load(() => {
+  jQuery('body').css(
+    'background-image',
+    `url('/wp-content/uploads/2018/08/shutterstock-219154477.jpg')`
+  )
+})
