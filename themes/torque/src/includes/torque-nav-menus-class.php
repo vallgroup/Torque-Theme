669cc0d220725d25733ffbd95f54b9bfc2618d1c
mattwills8 <matt_wills8@hotmail.co.uk>
2018-08-14 11:06:03 +0200

    update parent theme nav menu class to store primary menu filter handle as static var

--- themes/torque/src/includes/torque-nav-menus-class.php
+++ themes/torque/src/includes/torque-nav-menus-class.php
@@ -4,6 +4,8 @@ class Torque_Nav_Menus {
 
   public static $nav_menus_filter_handle = 'torque_nav_menus';
 
+  public static $nav_menus_primary_location_filter_handle = 'torque_nav_menus_primary_location_slug';
+
   public static function register_all() {
     register_nav_menus( self::get_menus() );
   }
@@ -29,7 +31,7 @@ class Torque_Nav_Menus {
    * if for some reason it has to be different
    */
   public static function get_default_primary_location_slug() {
-    return apply_filters('torque_nav_menus_primary_location_slug', 'primary');
+    return apply_filters( self::$nav_menus_primary_location_filter_handle, 'primary');
   }
 
   /**
