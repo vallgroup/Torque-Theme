692d329eb02815e1cee7c5e6ba3a3941b594e840
mattwills8 <matt_wills8@hotmail.co.uk>
2018-08-20 12:44:52 +0100

    update plugin boilerplate cpt class layout

--- boilerplates/torque-plugin/src/cpt/torque-plugin-cpt-class.php
+++ boilerplates/torque-plugin/src/cpt/torque-plugin-cpt-class.php
@@ -5,53 +5,66 @@
 class <torque_plugin_class_name>_CPT {
 
 	/**
+	 * Holds the example cpt object
+	 *
+	 * @var Object
+	 */
+	protected $example = null;
+
+	/**
+	 * Holds the labels needed to build the example custom post type.
+	 *
+	 * @var array
+	 */
+	public static $example_labels = array(
+			'singular'       => 'Example',
+			'plural'         => 'Examples',
+			'slug'           => 'torque-example',
+			'post_type_name' => 'torque_example',
+	);
+
+	/**
+	 * Holds options for the example custom post type
+	 *
+	 * @var array
+	 */
+	protected $example_options = array(
+		'supports' => array(
+			'title',
+			'editor',
+			'thumbnail',
+		),
+		'menu_icon'           => 'dashicons-businessman',
+	);
+
+	/**
 	 * register our post type and meta boxes
 	 */
 	function __construct() {
 		if ( class_exists( 'PremiseCPT' ) ) {
-			new PremiseCPT( array(
-				'plural'         => 'Examples',
-				'singular'       => 'Example',
-				'post_type_name' => 'torque_example',
-				'slug'           => 'torque-example'
-			), array(
-				'supports'             => array( 'title' ),
-				'menu_icon'            => 'dashicons-admin-post',
-				'public'               => false,
-				'show_ui'              => true,
-				'show_in_rest'         => true,
-			));
-
-			add_action( 'init', array( $this, 'register_mb' ) );
+			new PremiseCPT( self::$example_labels, $this->example_options );
 		}
-	}
 
-	/**
-	 * add the metaboxes to the post type
-	 *
-	 * @return void
-	 */
-	public function register_mb() {
-
-		pwp_add_metabox( 'Example Metabox', array( 'torque_example_mb' ), array(
-			'name_prefix' => 'torque_example_mb',
+		pwp_add_metabox(
+			'Example Meta',
+			array( self::$example_labels['post_type_name'] ),
 			array(
-				'context'     => 'post',
-				'type'        => 'text',
-				'name'        => '[example_field]',
-				'label'       => 'Example Field',
-				'placeholder' => 'Address, Zip Code or Place',
+				'name_prefix' => 'example_meta',
+				array(
+					'type'    => 'text',
+					'context' => 'post',
+					'name'    => '[tel]',
+					'label'   => 'Telephone',
+				),
+				array(
+					'type'    => 'text',
+					'context' => 'post',
+					'name'    => '[email]',
+					'label'   => 'Email',
+				),
 			),
-			array(
-				'context'     => 'post',
-				'type'        => 'wp_media',
-				'name'        => '[example_image_url]',
-				'label'       => 'Example Image',
-				'placeholder' => 'Enter icon url or click upload button',
-			),
-		), 'torque_example_mb');
-
-		// add more metaboxes here
+			'example_meta'
+		);
 	}
 }
 
