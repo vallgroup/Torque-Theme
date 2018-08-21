35c2726b145ca2ff8fa27ec1e252ba2b2cd1365c
mattwills8 <matt_wills8@hotmail.co.uk>
2018-08-21 19:49:38 +0100

    add pagination template

---
+++ themes/torque/src/parts/elements/pagination/pagination-buttons.php
@@ -0,0 +1,4 @@
+<div class="torque-pagination">
+    <?php previous_posts_link('<button class="torque-pagination-button prev-page">Previous Page</button>'); ?>
+    <?php next_posts_link('<button class="torque-pagination-button next-page">Next Page</button>'); ?>
+</div>
