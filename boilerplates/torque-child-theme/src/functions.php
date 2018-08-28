7ce9531f4dfda72eae293e95559ed946639254de
mattwills8 <matt_wills8@hotmail.co.uk>
2018-08-28 18:09:27 +0100

    add remove menus function to child theme boilerplate

--- boilerplates/torque-child-theme/src/functions.php
+++ boilerplates/torque-child-theme/src/functions.php
@@ -29,6 +29,36 @@ if ( class_exists( '<torque_child_theme_class_prefix>_Widgets' ) ) {
  }
 
 
+
+
+/**
+ * Admin settings
+ */
+
+ // remove menu items
+ function torque_remove_menus(){
+
+   //remove_menu_page( 'index.php' );                  //Dashboard
+   //remove_menu_page( 'edit.php' );                   //Posts
+   //remove_menu_page( 'upload.php' );                 //Media
+   //remove_menu_page( 'edit.php?post_type=page' );    //Pages
+   //remove_menu_page( 'edit-comments.php' );          //Comments
+   //remove_menu_page( 'themes.php' );                 //Appearance
+   //remove_menu_page( 'plugins.php' );                //Plugins
+   //remove_menu_page( 'users.php' );                  //Users
+   //remove_menu_page( 'tools.php' );                  //Tools
+   //remove_menu_page( 'options-general.php' );        //Settings
+
+ }
+ add_action( 'admin_menu', 'torque_remove_menus' );
+
+
+
+
+/**
+ * Enqueues
+ */
+
 // enqueue child styles after parent styles, both style.css and main.css
 // so child styles always get priority
 add_action( 'wp_enqueue_scripts', 'torque_enqueue_child_styles' );
