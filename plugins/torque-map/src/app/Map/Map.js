13950e79942160784a230271644b56eba3c2329c
mattwills8 <matt_wills8@hotmail.co.uk>
2018-08-28 18:58:58 +0100

    remove console logs from maps plugin

--- plugins/torque-map/src/app/Map/Map.js
+++ plugins/torque-map/src/app/Map/Map.js
@@ -1,38 +1,37 @@
-import React from 'react'
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
-import style from './Map.scss'
-import MapShape from './Shape'
+import React from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import style from "./Map.scss";
+import MapShape from "./Shape";
 
-import Geocode from '../Geocode/Geocode'
-import NearbySearch from '../NearbySearch/NearbySearch'
+import Geocode from "../Geocode/Geocode";
+import NearbySearch from "../NearbySearch/NearbySearch";
 
 export class TorqueMap extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       mapCenter: {}, // lat a& lng object
       selectedPlace: {},
       activeMarker: {},
       showingInfoWindow: false,
-      markers: [],
-    }
+      markers: []
+    };
 
-    this.map = null
-    this.placesServices = null
+    this.map = null;
+    this.placesServices = null;
   }
 
   componentWillMount() {
-    this.setMapCenterFromProps()
+    this.setMapCenterFromProps();
   }
 
   componentDidUpdate(prevProps) {
     // check if we have a new search term
     if (this.props.searchNearby !== prevProps.searchNearby) {
       // make sure we have the map and term before running the search
-      if (this.map
-        && this.props.searchNearby) {
-        this.nearbySearch()
+      if (this.map && this.props.searchNearby) {
+        this.nearbySearch();
       }
     }
   }
@@ -41,27 +40,28 @@ export class TorqueMap extends React.Component {
     this.setState({
       selectedPlace: props,
       activeMarker: marker,
-      showingInfoWindow: true,
-    })
+      showingInfoWindow: true
+    });
   }
 
   onMapClick(props) {
     if (this.state.showingInfoWindow) {
       this.setState({
         showingInfoWindow: false,
-        activeMarker: null,
-      })
+        activeMarker: null
+      });
     }
   }
 
   // pass a lat and lng object
   updateMapCenter(center) {
     this.setState({
-      mapCenter: center,
-    })
+      mapCenter: center
+    });
   }
 
   renderCenterMarker() {
+    console.log(this.props.centerMarker);
     return (
       <Marker
         onClick={this.onMarkerClick.bind(this)}
@@ -77,11 +77,11 @@ export class TorqueMap extends React.Component {
             scaledSize: new this.props.google.maps.Size(
               this.props.centerMarker.icon.width,
               this.props.centerMarker.icon.height
-            ),
+            )
           }
         }
       />
-    )
+    );
   }
 
   renderMarkers() {
@@ -97,26 +97,25 @@ export class TorqueMap extends React.Component {
               : marker.icon,
             anchor: new this.props.google.maps.Point(39, 54),
             size: new google.maps.Size(39, 54),
-            scaledSize: new google.maps.Size(39, 54),
+            scaledSize: new google.maps.Size(39, 54)
           }}
         />
-      )
-    })
+      );
+    });
   }
 
   render() {
-    // console.log(this.map)
-    // console.log(this.props)
-    // console.log(this.state)
     return (
       <div
         onClick={this.onMapClick.bind(this)}
-        className={`torque-map-container ${style.torqueMap}`}>
+        className={`torque-map-container ${style.torqueMap}`}
+      >
         <Map
           google={this.props.google}
           zoom={+this.props.zoom || 12}
           center={this.state.mapCenter}
-          ref={mapObject => (this.map = mapObject && mapObject.map)}>
+          ref={mapObject => (this.map = mapObject && mapObject.map)}
+        >
           {this.props.center &&
             this.props.centerMarker &&
             this.renderCenterMarker()}
@@ -127,51 +126,53 @@ export class TorqueMap extends React.Component {
 
           <InfoWindow
             marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}>
+            visible={this.state.showingInfoWindow}
+          >
             <div>
               <h3>{this.state.selectedPlace.name}</h3>
             </div>
           </InfoWindow>
         </Map>
       </div>
-    )
+    );
   }
 
   setMapCenterFromProps() {
     if (!this.props.center) {
-      return
+      return;
     }
 
-    if (!this.props.center.lat
-    	|| !this.props.center.lng) {
-    	this.geocode()
+    if (!this.props.center.lat || !this.props.center.lng) {
+      this.geocode();
     } else {
-      this.updateMapCenter(this.props.center)
+      this.updateMapCenter(this.props.center);
     }
   }
 
   async geocode() {
     const geoClient = new Geocode();
-    const coordinates = await geoClient.geocode({address:this.props.center})
-    this.updateMapCenter(coordinates)
+    const coordinates = await geoClient.geocode({ address: this.props.center });
+    this.updateMapCenter(coordinates);
   }
 
   async nearbySearch() {
-    const searchClient = new NearbySearch(this.map)
+    const searchClient = new NearbySearch(this.map);
     const results = await searchClient.search({
       keyword: this.props.searchNearby,
       location: this.state.mapCenter,
-      radius: 1000,
-    })
+      radius: 1000
+    });
     // add markers and call our callback
-    this.setState({ markers: results })
-    if (this.props.onNearbySearch &&
-      'function' === typeof this.props.onNearbySearch) {
-      this.props.onNearbySearch(results, this.state.mapCenter)
+    this.setState({ markers: results });
+    if (
+      this.props.onNearbySearch &&
+      "function" === typeof this.props.onNearbySearch
+    ) {
+      this.props.onNearbySearch(results, this.state.mapCenter);
     }
   }
 }
 
 export default GoogleApiWrapper(props => ({
-  apiKey: props.apiKey,
-}))(TorqueMap)
+  apiKey: props.apiKey
+}))(TorqueMap);
