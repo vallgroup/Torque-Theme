aafdb0ce00dcfa7837e738c22d3a9bbdc59c07a7
mattwills8 <matt_wills8@hotmail.co.uk>
2018-09-13 12:21:45 +0100

    add a posts wrapper div

--- plugins/torque-filtered-loop/src/app/Posts/Posts.js
+++ plugins/torque-filtered-loop/src/app/Posts/Posts.js
@@ -4,19 +4,23 @@ import { Template_0 } from "./Templates";
 
 class Posts extends React.PureComponent {
   render() {
-    return this.props.posts.map((post, index) => {
-      switch (this.props.loopTemplate) {
-        case "template-0":
-        default:
-          return (
-            <Template_0
-              key={index}
-              post={post}
-              parentId={this.props.parentId}
-            />
-          );
-      }
-    });
+    return (
+      <div className={"posts-wrapper"}>
+        {this.props.posts.map((post, index) => {
+          switch (this.props.loopTemplate) {
+            case "template-0":
+            default:
+              return (
+                <Template_0
+                  key={index}
+                  post={post}
+                  parentId={this.props.parentId}
+                />
+              );
+          }
+        })}
+      </div>
+    );
   }
 }
 
