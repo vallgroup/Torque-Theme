c939f63f8a20a0ad79a72e5ebbdba8b6e4597762
mattwills8 <matt_wills8@hotmail.co.uk>
2018-09-14 13:15:22 +0100

    configure US States post type and add state selector metabox

--- plugins/torque-us-states/src/cpt/torque-us-states-cpt-class.php
+++ plugins/torque-us-states/src/cpt/torque-us-states-cpt-class.php
@@ -2,69 +2,86 @@
 /**
  * Register the torque cpt and it's meta boxes
  */
+
+require_once( Torque_US_States_PATH . 'includes/torque-us-states-functions-class.php');
+
 class Torque_US_States_CPT {
 
 	/**
-	 * Holds the example cpt object
+	 * Holds the us_states cpt object
 	 *
 	 * @var Object
 	 */
-	protected $example = null;
+	protected $us_states = null;
 
 	/**
-	 * Holds the labels needed to build the example custom post type.
+	 * Holds the labels needed to build the us_states custom post type.
 	 *
 	 * @var array
 	 */
-	public static $example_labels = array(
-			'singular'       => 'Example',
-			'plural'         => 'Examples',
-			'slug'           => 'torque-example',
-			'post_type_name' => 'torque_example',
+	public static $us_states_labels = array(
+			'singular'       => 'State',
+			'plural'         => 'States',
+			'slug'           => 'torque-us-state',
+			'post_type_name' => 'torque_us_state',
 	);
 
 	/**
-	 * Holds options for the example custom post type
+	 * Holds options for the us_states custom post type
 	 *
 	 * @var array
 	 */
-	protected $example_options = array(
+	protected $us_states_options = array(
 		'supports' => array(
 			'title',
 			'editor',
-			'thumbnail',
 		),
-		'menu_icon'           => 'dashicons-businessman',
+		'menu_icon'           => 'dashicons-location-alt',
 	);
 
 	/**
 	 * register our post type and meta boxes
 	 */
-	function __construct() {
+	public function __construct() {
 		if ( class_exists( 'PremiseCPT' ) ) {
-			new PremiseCPT( self::$example_labels, $this->example_options );
+			new PremiseCPT( self::$us_states_labels, $this->us_states_options );
 		}
 
 		pwp_add_metabox(
-			'Example Meta',
-			array( self::$example_labels['post_type_name'] ),
 			array(
-				'name_prefix' => 'example_meta',
+				'title'   => 'Assign a State',
+				'context' => 'side',
+				'priority' => 'high',
+			),
+			array( self::$us_states_labels['post_type_name'] ),
+			array(
 				array(
-					'type'    => 'text',
+					'type'    => 'select',
 					'context' => 'post',
-					'name'    => '[tel]',
-					'label'   => 'Telephone',
-				),
-				array(
-					'type'    => 'text',
-					'context' => 'post',
-					'name'    => '[email]',
-					'label'   => 'Email',
+					'name'    => 'state_code',
+					'label'   => 'Unassigned States',
+					'options'	=> $this->get_state_options()
 				),
 			),
-			'example_meta'
+			'state_code'
 		);
+	}
+
+	private function get_state_options() {
+		$unassigned_states = Torque_US_States_Functions::get_unassigned_states();
+
+		// we need to add the current post's selected state back in to the dropdown
+		if ( isset($_GET['post']) ) {
+
+			$assigned_state_code = Torque_US_States_Functions::get_state_post_assigned_state_code( $_GET['post'] );
+
+			if ( $assigned_state_code ) {
+				$unassigned_states[$assigned_state_code] = Torque_US_States_Functions::$states[$assigned_state_code];
+			}
+		}
+
+
+		return array_flip($unassigned_states);
 	}
 }
 
