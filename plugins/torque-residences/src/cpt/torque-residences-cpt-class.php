e90563f6424f17f4a4af279bc9a9812b35aaa36b
Matt Wills <matt_wills8@hotmail.co.uk>
2018-09-18 19:11:52 +0100

    Squash - Add US States Plugin + Add Residences Plugin (#16)
    * add us states plugin and complete README instructions
    
    * add helper class for interacting with US states post type
    
    * configure US States post type and add state selector metabox
    
    * add filter allowing theme to easily add a state selector metabox to any post type
    
    * add API routes for getting US States related WP options
    
    * add option for storing which post types have the US States selector enabled
    
    * Add a post types dropdown to the tinyMCE editor to allow the user to choose a post type for the US States app loop
    
    * fix bug in tinymce form - make sure the shortcode is populating properly
    
    * add post type to shortcode default atts
    
    * add endpoint for getting loop by state
    
    * add route for getting state posts
    
    * add ids and class names specific to each state to all map svg paths
    
    * add network request for getting state posts and add styling for not empty and selected states
    
    * add functionality for updating currentState when clicking a valid state
    
    * add network request and state for loop
    
    * add loop section markup
    
    * add options to tinyMCE for allowing the user to select a source for the loop post link
    
    * add the custom link to the front end loop
    
    * add instructional text input to tinyMCE and to front end
    
    * add residences plugin with residences CPT
    
    * add residences plugin to workspaces cli
    
    * update us states hooks to make sure we can use user added post types
    
    * fix bug - getting link source meta we want to return the meta value not an array
    
    * fix bug in tinyMCE form - dropdowns werent saving selected value
    
    * add instructions to residence cpt to make sure user adds http:// to external links
    
    * fix bug where link source meta key wasnt making it through to the front end
    
    * add states list to us states map for optional other interface
    
    * add default colours to states list
    
    * add some updates to app layout and content
    
    * update torque states readme

---
+++ plugins/torque-residences/src/cpt/torque-residences-cpt-class.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Register the torque cpt and it's meta boxes
+ */
+class Torque_Residences_CPT {
+
+	/**
+	 * Holds the residences cpt object
+	 *
+	 * @var Object
+	 */
+	protected $residences = null;
+
+	/**
+	 * Holds the labels needed to build the residences custom post type.
+	 *
+	 * @var array
+	 */
+	public static $residences_labels = array(
+			'singular'       => 'Residence',
+			'plural'         => 'Residences',
+			'slug'           => 'torque-residences',
+			'post_type_name' => 'torque_residences',
+	);
+
+	/**
+	 * Holds options for the residences custom post type
+	 *
+	 * @var array
+	 */
+	protected $residences_options = array(
+		'supports' => array(
+			'title',
+			'editor',
+			'thumbnail',
+		),
+		'menu_icon'           => 'dashicons-admin-multisite',
+	);
+
+	/**
+	 * register our post type and meta boxes
+	 */
+	function __construct() {
+		if ( class_exists( 'PremiseCPT' ) ) {
+			new PremiseCPT( self::$residences_labels, $this->residences_options );
+		}
+
+		pwp_add_metabox(
+			'Residence Website',
+			array( self::$residences_labels['post_type_name'] ),
+			array(
+				array(
+					'type'    => 'text',
+					'context' => 'post',
+					'name'    => 'residence_website_url',
+					'label'   => 'Website URL (if http:// is not included then this will be treated as a relative url to the current site)',
+				),
+			),
+			'residence_website_url'
+		);
+	}
+}
+
+?>
---
+++ plugins/torque-us-states/src/api/controllers/torque-us-states-options-controller-class.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once( get_template_directory() . '/api/responses/torque-api-responses-class.php');
+require_once( get_template_directory() . '/includes/validation/torque-validation-class.php');
+
+class Torque_US_States_Options_Controller {
+
+	public static function get_options_args() {
+		return array();
+	}
+
+	protected $request = null;
+
+	function __construct( $request ) {
+
+		$this->request = $request;
+	}
+
+	public function get_options() {
+		try {
+			$options = array(
+				'post_types' => get_option( Torque_US_States_CPT::$POST_TYPES_WITH_STATE_ASSIGNER_OPTION_HANDLE ),
+				'link_source_options' => get_option( Torque_US_States_CPT::$LOOP_LINK_SOURCE_META_KEY_OPTION_HANDLE )
+			);
+
+			if ($options && sizeof($options)) {
+        return Torque_API_Responses::Success_Response( array(
+          'options'	=> $options
+        ) );
+			}
+
+			return Torque_API_Responses::Failure_Response( array(
+				'options'	=> []
+			));
+
+		} catch (Exception $e) {
+			return Torque_API_Responses::Error_Response( $e );
+		}
+	}
+}
