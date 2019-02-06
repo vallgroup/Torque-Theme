5f8e39db8792e64e75daf47581240557a576e267
mattwills8 <matt_wills8@hotmail.co.uk>
2019-02-05 22:27:37 -0600

    update - Fix failing postcss and add browserslist for browser support

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,3 @@
-const autoprefixer = require('autoprefixer')
-
 module.exports = {
-  plugins: [
-    autoprefixer({
-      browsers: 'last 2 versions',
-    }),
-  ],
-}
+  plugins: [require("autoprefixer")]
+};
