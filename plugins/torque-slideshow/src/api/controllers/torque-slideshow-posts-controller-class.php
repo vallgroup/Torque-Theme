c48ce519f1efc2c636f4db9d057cd85e76937bf3
mattwills8 <matt_wills8@hotmail.co.uk>
2019-03-19 17:35:39 -0600

    add post slideshow slide template

--- plugins/torque-slideshow/src/api/controllers/torque-slideshow-posts-controller-class.php
+++ plugins/torque-slideshow/src/api/controllers/torque-slideshow-posts-controller-class.php
@@ -15,9 +15,13 @@ class Torque_Slideshow_Posts_Controller {
 
 	protected $request = null;
 
-	function __construct( $request ) {
+	private $post_type_labels = array();
 
+	private $taxonomies = [];
+
+	function __construct( $request ) {
 		$this->request = $request;
+		$this->taxonomies = get_taxonomies(array('public' => true),'names');
 	}
 
 	public function get_posts() {
@@ -28,30 +32,12 @@ class Torque_Slideshow_Posts_Controller {
 			) );
 
 			if ($query->have_posts()) {
-				$posts = [];
-
-				foreach ($query->posts as $post) {
-					$post->meta =
-					array_filter(
-						array_merge(
-							// get all meta from both WP and ACF
-							get_post_meta($post->ID),
-							get_fields($post->ID)
-						),
-						function($meta_key) {
-							// hide private meta
-							return $meta_key[0] !== '_';
-						},
-						ARRAY_FILTER_USE_KEY
-					);
-
-					$post->thumbnail = get_the_post_thumbnail_url($post->ID, 'large');
-
-					$posts[] = $post;
+				foreach ($query->posts as &$post) {
+					$this->setup_post_shape($post);
 				}
 
         return Torque_API_Responses::Success_Response( array(
-          'data'	=> $posts
+          'data'	=> $query->posts
         ) );
 			}
 
@@ -62,5 +48,44 @@ class Torque_Slideshow_Posts_Controller {
 		} catch (Exception $e) {
 			return Torque_API_Responses::Error_Response( $e );
 		}
+	}
+
+	private function setup_post_shape( $post ) {
+		$post->meta =
+		array_filter(
+			array_merge(
+				// get all meta from both WP and ACF
+				get_post_meta($post->ID),
+				get_fields($post->ID)
+			),
+			function($meta_key) {
+				// hide private meta
+				return $meta_key[0] !== '_';
+			},
+			ARRAY_FILTER_USE_KEY
+		);
+
+		$post->thumbnail = get_the_post_thumbnail_url($post->ID, 'large');
+
+		$post->post_type_label = $this->get_post_type_label($post->post_type);
+
+		$post->permalink = get_post_permalink($post->ID);
+
+		$terms = wp_get_post_terms($post->ID, array_keys($this->taxonomies));
+		$post->terms = array_map(function($term) { return $term->name; }, $terms);
+
+		return $post;
+	}
+
+	private function get_post_type_label( $post_type ) {
+		if ($this->post_type_labels[$post_type]) {
+			return $this->post_type_labels[$post_type];
+		}
+
+		$post_type = get_post_type_object( $post_type );
+		$post_type_label = $post_type->label;
+
+		$this->post_type_labels[$post_type] = $post_type_label;
+		return $post_type_label;
 	}
 }
--- plugins/torque-slideshow/src/app/PostSlideshow/PostSlideshow.js
+++ plugins/torque-slideshow/src/app/PostSlideshow/PostSlideshow.js
@@ -11,18 +11,62 @@ const PostSlideshow = ({ site, postIds, interval }) => {
   const params = useMemo(() => ({ ids: postIds }), [postIds]);
   useSlideshowFetch(site, "posts", params, setPosts);
 
-  console.log(posts);
-
   return (
-    <Slideshow
-      length={posts.length}
-      interval={interval}
-      slideTemplate={slide => (
-        <div className={classnames(styles.post_slide, "post-slide")}>
-          {posts[slide]?.post_title}
-        </div>
-      )}
-    />
+    posts.length > 0 && (
+      <Slideshow
+        length={posts.length}
+        interval={interval}
+        slideTemplate={slide => {
+          const post = posts[slide];
+
+          return (
+            <div className={classnames(styles.post_slide, "post-slide")}>
+              {post.thumbnail && <img src={post.thumbnail} />}
+
+              <h3>{post.post_title}</h3>
+
+              <div className="slideshow-post-excerpt slideshow-post-field">
+                {post.post_excerpt || post.post_content}
+              </div>
+
+              {Object.keys(post.meta).map(metaKey => {
+                const meta = post.meta[metaKey];
+
+                if (typeof meta !== "string") return null;
+
+                return (
+                  <div
+                    key={metaKey}
+                    className={classnames(
+                      "slideshow-post-field",
+                      `slideshow-${metaKey}`
+                    )}
+                  >
+                    {post.meta[metaKey]}
+                  </div>
+                );
+              })}
+
+              <a
+                href={post.permalink}
+                target="_blank"
+                referrer="noreferrer noopener"
+              >
+                <button>{`Visit ${post.post_type_label}`}</button>
+              </a>
+
+              <div className="slideshow-post-terms">
+                {post.terms.map(term => (
+                  <div key={term} className="post_term">
+                    {term}
+                  </div>
+                ))}
+              </div>
+            </div>
+          );
+        }}
+      />
+    )
   );
 };
 
