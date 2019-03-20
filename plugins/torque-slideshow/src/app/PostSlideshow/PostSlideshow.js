2d3e1cd03a1efc1a32126f93f11105785373af3d
mattwills8 <matt_wills8@hotmail.co.uk>
2019-03-19 19:03:02 -0600

    small changes to post slideshow template

--- plugins/torque-slideshow/src/app/PostSlideshow/PostSlideshow.js
+++ plugins/torque-slideshow/src/app/PostSlideshow/PostSlideshow.js
@@ -20,46 +20,51 @@ const PostSlideshow = ({ site, postIds, interval }) => {
         slideTemplate={post => {
           return (
             <div className={classnames(styles.post_slide, "post-slide")}>
-              {post.thumbnail && <img src={post.thumbnail} />}
+              <div
+                style={{ backgroundImage: `url(${post.thumbnail})` }}
+                className="post-image"
+              />
 
-              <h3>{post.post_title}</h3>
+              <div className="post-details">
+                <h3>{post.post_title}</h3>
 
-              <div className="slideshow-post-excerpt slideshow-post-field">
-                {post.post_excerpt || post.post_content}
-              </div>
+                <div className="slideshow-post-excerpt slideshow-post-field">
+                  {post.post_excerpt || post.post_content}
+                </div>
 
-              {Object.keys(post.meta).map(metaKey => {
-                const meta = post.meta[metaKey];
+                {Object.keys(post.meta).map(metaKey => {
+                  const meta = post.meta[metaKey];
 
-                if (typeof meta !== "string") return null;
+                  if (typeof meta !== "string") return null;
 
-                return (
-                  <div
-                    key={metaKey}
-                    className={classnames(
-                      "slideshow-post-field",
-                      `slideshow-${metaKey}`
-                    )}
-                  >
-                    {post.meta[metaKey]}
-                  </div>
-                );
-              })}
+                  return (
+                    <div
+                      key={metaKey}
+                      className={classnames(
+                        "slideshow-post-field",
+                        `slideshow-${metaKey}`
+                      )}
+                    >
+                      {post.meta[metaKey]}
+                    </div>
+                  );
+                })}
 
-              <a
-                href={post.permalink}
-                target="_blank"
-                referrer="noreferrer noopener"
-              >
-                <button>{`Visit ${post.post_type_label}`}</button>
-              </a>
+                <a
+                  href={post.permalink}
+                  target="_blank"
+                  referrer="noreferrer noopener"
+                >
+                  <button>{`Visit ${post.post_type_label}`}</button>
+                </a>
 
-              <div className="slideshow-post-terms">
-                {post.terms.map(term => (
-                  <div key={term} className="post_term">
-                    {term}
-                  </div>
-                ))}
+                <div className="slideshow-post-terms">
+                  {post.terms.map(term => (
+                    <div key={term} className="post-term">
+                      {term}
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
           );
