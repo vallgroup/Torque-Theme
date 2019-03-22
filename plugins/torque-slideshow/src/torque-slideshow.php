8bed24d5f0d2494e0187e8519d3b29753a32dd48
mattwills8 <matt_wills8@hotmail.co.uk>
2019-03-22 17:41:00 -0600

    update README and version

--- plugins/torque-slideshow/src/torque-slideshow.php
+++ plugins/torque-slideshow/src/torque-slideshow.php
@@ -4,7 +4,7 @@
  /**
   * Plugin Name: Torque Slideshow
   * Description:
-  * Version:     2.0.0
+  * Version:     2.0.1
   * Author:      Torque
   * Author URI:  https://torque.digital
   * License:     GPL
