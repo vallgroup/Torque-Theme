d2ed03768e71ef3525c319184f58faed862deecd
mattwills8 <matt_wills8@hotmail.co.uk>
2019-03-25 15:42:39 -0600

    add TabsACF custom filter

--- plugins/torque-filtered-loop/src/app/Filters/CustomFilters/TabsACF.js
+++ plugins/torque-filtered-loop/src/app/Filters/CustomFilters/TabsACF.js
@@ -1,16 +1,26 @@
 import React, { memo } from "React";
 import Filters from "..";
-import { useWPTerms } from "../../hooks";
+import { useACFSelectChoices } from "../../hooks";
 
 const TabsACF = ({ site, value, onChange, args }) => {
+  if (typeof args !== "string") {
+    console.warn(
+      `TabsACF: expected args to be an acf select field id but got ${args}`
+    );
+    return null;
+  }
+
+  const choices = useACFSelectChoices(site, args);
+
   return (
     <Filters
       className="torque-acf-tabs-filters"
-      terms={[]}
+      terms={choices}
       activeTerm={0}
       updateActiveTerm={onChange}
       hideAllOption
     />
   );
 };
+
 export default memo(TabsACF);
--- plugins/torque-filtered-loop/src/app/hooks/index.js
+++ plugins/torque-filtered-loop/src/app/hooks/index.js
@@ -1,5 +1,6 @@
 export { default as useWPTerms } from "./useWPTerms";
 export { default as useWPPosts } from "./useWPPosts";
 export { default as useParentId } from "./useParentId";
+export { default as useACFSelectChoices } from "./useACFSelectChoices";
 
 export { default as useCustomFilterSettings } from "./useCustomFilterSettings";
