cbc13a59db15b3d9bf355338893e88c18c409a77
Al Notara <al@vallgroup.com>
2019-05-21 13:01:49 -0700

    Fix: close the burger menu when clicking outside open menu

--- themes/torque/src/js/header/toggle-burger-menu.js
+++ themes/torque/src/js/header/toggle-burger-menu.js
@@ -6,8 +6,22 @@
 
     navToggle.on("click", e => {
       e.preventDefault();
+
+      /* Toggle the menu active/inactive */
       nav.toggleClass("active");
       navToggle.toggleClass("active");
+
+      /* Add event listener to close menu when */
+      if ( navToggle.hasClass('active') ) {
+        $('main').click(function(){
+          /* When the 'main' element is clicked, toggle the menu by removing '/'.active' */
+          nav.toggleClass("active");
+          navToggle.toggleClass("active");
+
+          /* Unbind the event listener for 'main' element */
+          $('main').off('click');
+        });
+      }
     });
   });
 })(jQuery);
