5c7d74b6c246e9e46113eb09cd5240628e9c445a
Al Notara <al@vallgroup.com>
2019-08-15 14:26:09 -0700

    Added ACF php-to-json export tool

--- themes/torque/src/includes/acf/acf-init.php
+++ themes/torque/src/includes/acf/acf-init.php
@@ -9,3 +9,16 @@ if( !function_exists('tq_theme_prefix_acf_add_local_field_groups') ):
   }
 endif;
 add_action('acf/init', 'tq_theme_prefix_acf_add_local_field_groups');
+
+
+/**
+ * Intro: Adds a admin menu item to export ACF Fields from PHP to JSON
+ * Steps:
+ *  1) Un-comment the 'require' below
+ *  2) Create folder 'acf-json' in child-theme root dir (/wp-content/themes/child-theme/acf-json/)
+ *  3) Run the feature via Custom Fields -> PHP to JSON -> Convert Field Groups
+ *  4) Select the JSON file containing the Custom Fields you'd like to import
+ *  5) Import via Custom Fields -> Tools -> Import Field Groups
+ *  6) When done delete the 'acf-json' folder + contents, and comment 'require' below
+ */
+require('acf-php-to-json.php');
