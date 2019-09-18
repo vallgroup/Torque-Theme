3e54fedafab0775aa2bf7b61fc562913416624ad
Mario Vallejo <mario@vallgroup.com>
2019-09-18 08:09:00 -0500

    set global styles for interra markeing sutomation maps

--- plugins/interra-marketing-automation/src/interra-marketing-automation.php
+++ plugins/interra-marketing-automation/src/interra-marketing-automation.php
@@ -80,10 +80,191 @@ class Interra_Marketing_Automation {
 
 		add_filter( 'torque_map_api_key', array( $this, 'gmap_api_key') );
 
+		add_filter( 'torque_map_default_styles', array( $this, 'get_map_styles') );
+
 	}
 
 	public function gmap_api_key() {
 		return 'AIzaSyBbzylAdZ7vqtFdVbfxK2Vkwt5-lCm1F30';
+	}
+
+	public function get_map_styles() {
+		return '[
+		    {
+		        "featureType": "water",
+		        "elementType": "geometry",
+		        "stylers": [
+		            {
+		                "color": "#e9e9e9"
+		            },
+		            {
+		                "lightness": 17
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "landscape",
+		        "elementType": "geometry",
+		        "stylers": [
+		            {
+		                "color": "#f5f5f5"
+		            },
+		            {
+		                "lightness": 20
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "road.highway",
+		        "elementType": "geometry.fill",
+		        "stylers": [
+		            {
+		                "color": "#ffffff"
+		            },
+		            {
+		                "lightness": 17
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "road.highway",
+		        "elementType": "geometry.stroke",
+		        "stylers": [
+		            {
+		                "color": "#ffffff"
+		            },
+		            {
+		                "lightness": 29
+		            },
+		            {
+		                "weight": 0.2
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "road.arterial",
+		        "elementType": "geometry",
+		        "stylers": [
+		            {
+		                "color": "#ffffff"
+		            },
+		            {
+		                "lightness": 18
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "road.local",
+		        "elementType": "geometry",
+		        "stylers": [
+		            {
+		                "color": "#ffffff"
+		            },
+		            {
+		                "lightness": 16
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "poi",
+		        "elementType": "geometry",
+		        "stylers": [
+		            {
+		                "color": "#f5f5f5"
+		            },
+		            {
+		                "lightness": 21
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "poi.park",
+		        "elementType": "geometry",
+		        "stylers": [
+		            {
+		                "color": "#dedede"
+		            },
+		            {
+		                "lightness": 21
+		            }
+		        ]
+		    },
+		    {
+		        "elementType": "labels.text.stroke",
+		        "stylers": [
+		            {
+		                "visibility": "on"
+		            },
+		            {
+		                "color": "#ffffff"
+		            },
+		            {
+		                "lightness": 16
+		            }
+		        ]
+		    },
+		    {
+		        "elementType": "labels.text.fill",
+		        "stylers": [
+		            {
+		                "saturation": 36
+		            },
+		            {
+		                "color": "#333333"
+		            },
+		            {
+		                "lightness": 40
+		            }
+		        ]
+		    },
+		    {
+		        "elementType": "labels.icon",
+		        "stylers": [
+		            {
+		                "visibility": "off"
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "transit",
+		        "elementType": "geometry",
+		        "stylers": [
+		            {
+		                "color": "#f2f2f2"
+		            },
+		            {
+		                "lightness": 19
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "administrative",
+		        "elementType": "geometry.fill",
+		        "stylers": [
+		            {
+		                "color": "#fefefe"
+		            },
+		            {
+		                "lightness": 20
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "administrative",
+		        "elementType": "geometry.stroke",
+		        "stylers": [
+		            {
+		                "color": "#fefefe"
+		            },
+		            {
+		                "lightness": 17
+		            },
+		            {
+		                "weight": 1.2
+		            }
+		        ]
+		    }
+		]';
 	}
 
 	public function filter_ima_page_template( $template ) {
