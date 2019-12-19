05134458d52d7a7251e67f1a6b027c6055003f26
Al Notara <al@vallgroup.com>
2019-12-19 14:48:24 -0800

    Cleaned up plugin code

--- plugins/torque-image-grid/src/app/components/GridItem/GridItem.js
+++ plugins/torque-image-grid/src/app/components/GridItem/GridItem.js
@@ -9,25 +9,6 @@ const GridItem = ({
   image,
   imagesPerRow
 }) => {
-  
-  // // Not in-use, as using ACF 'link' field which includes target
-  // // add to element: onClick={((e) => handleImageClick(e, image.media_link.url))}
-  // function handleImageClick (e, imageLink) {
-  //   // stop default behaviour
-  //   e.preventDefault()
-  //   e.stopPropagation()
-  //   const currHostname = location.hostname // window url hostname
-  //   const imgLinkHostname = e.target.parentElement.hostname // element url hostname
-  //   // console.log('currHostname', currHostname)
-  //   // console.log('imgLinkHostname', imgLinkHostname)
-  //   if ( currHostname === imgLinkHostname ) {
-  //     // if of some origin, open in same tab/window
-  //     window.open(imageLink, '_self')
-  //   } else {
-  //     // open in new tab/window
-  //     window.open(imageLink, '_blank')
-  //   }
-  // }
 
   return (
     <ImageGridImageWrapper
@@ -55,3 +36,4 @@ const GridItem = ({
 }
 
 export default GridItem
+ 
