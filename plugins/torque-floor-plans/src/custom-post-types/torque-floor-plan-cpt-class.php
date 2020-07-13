633b25b12ab2d3b09761d240e8155192a3fb0c98
Mario Vallejo <mario@vallgroup.com>
2020-07-13 11:06:47 -0500

    adds new acf fields to floorplan categories and removes the old fields

--- plugins/torque-floor-plans/src/custom-post-types/torque-floor-plan-cpt-class.php
+++ plugins/torque-floor-plans/src/custom-post-types/torque-floor-plan-cpt-class.php
@@ -53,60 +53,55 @@ class Torque_Floor_Plan_CPT {
 	public function add_acf_metaboxes() {
 		if( function_exists('acf_add_local_field_group') ):
 
-			acf_add_local_field_group(array(
-				'key' => 'group_5ee16e7bd04f0',
-				'title' => 'Floorplan Options',
-				'fields' => array(
-					array(
-						'key' => self::$FLOORPLAN_CATEGORY_ACF_KEY,
-						'label' => 'Floorplan Category',
-						'name' => 'floorplan_category',
-						'type' => 'select',
-						'instructions' => '',
-						'required' => 0,
-						'conditional_logic' => 0,
-						'wrapper' => array(
-							'width' => '',
-							'class' => '',
-							'id' => '',
-						),
-						'choices' => array(
-							'central-townhomes' => 'Central Townhomes',
-							'fairview-townhomes' => 'Fairview Townhomes',
-							'lynn-townhomes' => 'Lynn Townhomes',
-							'minor-townhomes' => 'Minor Townhomes',
-						),
-						'default_value' => array(
-							0 => 'central-townhomes',
-						),
-						'allow_null' => 0,
-						'multiple' => 0,
-						'ui' => 1,
-						'ajax' => 0,
-						'return_format' => 'value',
-						'placeholder' => '',
-					),
-				),
-				'location' => array(
-					array(
-						array(
-							'param' => 'post_type',
-							'operator' => '==',
-							'value' => 'floor_plan',
-						),
-					),
-				),
-				'menu_order' => 0,
-				'position' => 'normal',
-				'style' => 'default',
-				'label_placement' => 'top',
-				'instruction_placement' => 'label',
-				'hide_on_screen' => '',
-				'active' => 1,
-				'description' => '',
-			));
-			
-			endif;
+acf_add_local_field_group(array(
+	'key' => 'group_5f08df14dd16f',
+	'title' => 'Floorplan Category Options',
+	'fields' => array(
+		array(
+			'key' => 'field_5f08df8cd93cb',
+			'label' => 'Thumbnail',
+			'name' => 'thumbnail',
+			'type' => 'image',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'preview_size' => 'medium',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'taxonomy',
+				'operator' => '==',
+				'value' => 'floor_plan_cat',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => 1,
+	'description' => '',
+));
+
+endif;
 	}
 
 	public function add_metaboxes() {
