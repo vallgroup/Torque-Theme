544b57965161e671a3a891d6ddf5a0f9633d64d3
Jorge Cortez <jorge@vallgroup.com>
2023-06-08 15:39:04 -0400

    PHOT-124 add tracking event job_application

--- plugins/torque-recruiterbox/src/app/components/Opening/Opening.js
+++ plugins/torque-recruiterbox/src/app/components/Opening/Opening.js
@@ -7,6 +7,12 @@ const Opening = ({opening}) => {
   const _title = opening.title;
   const _address = opening.location.name;
 
+  function handleClick(e) {
+    if (spectra) {
+      spectra.logEvent('job_application');
+    }
+  }
+
   return !objEmpty(opening) ?
     <div className={'opening-container'}>
       <a
@@ -14,6 +20,7 @@ const Opening = ({opening}) => {
         href={_url || ''}
         target={'_blank'}
         rel={'noopener noreferrer'}
+        onClick={handleClick}
       >
         <span className={'opening-title'}>{_title}</span>
         <span className={'opening-address'}>{_address}</span>
